38523ed0326fb27593eaf3ef6e6d684053b40a2a
Infocatcher <infocatcher.dev.null@gmail.com>
2014-01-07 19:45:17 +0400

    Decrease default value for "downloadsMaxRetentionDays" We don't want load and parse huge downloads.json at startup (#5)

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -18,7 +18,7 @@ pref("extensions.downloadPanelTweaker.overrideShowAllDownloads.private", 0);
 // 4 - open downloads library
 
 pref("extensions.downloadPanelTweaker.dontRemoveFinishedDownloads", true);
-pref("extensions.downloadPanelTweaker.downloadsMaxRetentionDays", 21);
+pref("extensions.downloadPanelTweaker.downloadsMaxRetentionDays", 7);
 // Only for Firefox 26+, store finished downloads at least this time (in days)
 pref("extensions.downloadPanelTweaker.fixWrongTabsOnTopAttribute", true);
 
