Infocatcher/Download_Panel_Tweaker

commit bf01d217c5d23046b9eea6fd90b857bf023f7071

Move code around download actions into separate file (#24)

bootstrap.js
@@ -182,6 +182,12 @@ var dpTweaker = {
 		delete this.de;
 		return this.de = downloadsEnhancements;
 	},
+	get da() {
+		_log("Load downloadsActions.js");
+		Services.scriptloader.loadSubScript("chrome://downloadpaneltweaker/content/downloadsActions.js");
+		delete this.da;
+		return this.da = downloadsActions;
+	},
 	get sss() {
 		delete this.sss;
 		return this.sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
@@ -507,11 +513,11 @@ var dpTweaker = {
 		if(curTrg.getAttribute && curTrg.hasAttribute("downloadPanelTweaker-command")) {
 			var cmd = curTrg.getAttribute("downloadPanelTweaker-command");
 			if(cmd == "clearDownloads")
-				this.clearDownloads(e.target);
+				this.da.clearDownloads(e.target);
 			else if(cmd == "copyReferrer")
-				this.copyReferrer(e.target);
+				this.da.copyReferrer(e.target);
 			else if(cmd == "removeFile")
-				this.removeFile(e.target);
+				this.da.removeFile(e.target);
 		}
 		else if(e.target.id == "Tools:Downloads") {
 			if(e.sourceEvent && e.sourceEvent.target.nodeName != "key")
@@ -547,9 +553,9 @@ var dpTweaker = {
 		}.bind(this), 0);
 	},
 	updateDlItemTooltip: function(trg) {
-		var dlController = this.getDlController(trg);
+		var dlController = this.da.getDlController(trg);
 		var dataItem = dlController.dataItem;
-		var path = this.getDataItemPath(dataItem);
+		var path = this.da.getDataItemPath(dataItem);
 		var tt = trg.getAttribute("tooltiptext") || "";
 		trg.setAttribute(this.origTtAttr, tt);
 		trg.setAttribute("tooltiptext", path);
@@ -591,11 +597,11 @@ var dpTweaker = {
 			return;
 		var ok;
 		switch(cmd) {
-			case 1: ok = this.toggleDownloadPanel(window);  break;
-			case 2: ok = this.showDownloadWindow(window);   break;
-			case 3: ok = this.openDownloadsTab(window);     break;
-			case 4: ok = this.openDownloadsLibrary(window); break;
-			case 5: ok = this.toggleDownloadsSidebar(window); break;
+			case 1: ok = this.da.toggleDownloadPanel(window);  break;
+			case 2: ok = this.da.showDownloadWindow(window);   break;
+			case 3: ok = this.da.openDownloadsTab(window);     break;
+			case 4: ok = this.da.openDownloadsLibrary(window); break;
+			case 5: ok = this.da.toggleDownloadsSidebar(window); break;
 			default: return;
 		}
 		if(ok == false)
@@ -606,158 +612,18 @@ var dpTweaker = {
 	panelClick: function(e) {
 		if(e.button != 1 || !prefs.get("middleClickToRemoveFromPanel"))
 			return;
-		var dlController = this.getDlController(e.target);
+		var dlController = this.da.getDlController(e.target);
 		if(!dlController)
 			return;
 		this.removeFromPanel(dlController, prefs.get("middleClickToRemoveFromPanel.clearHistory"));
 		this.stopEvent(e);
 	},
-	getDlNode: function(node) {
-		for(; node; node = node.parentNode) {
-			var ln = node.localName;
-			if(ln == "richlistitem") {
-				if(node.getAttribute("type") == "download")
-					return node;
-				break;
-			}
-			else if(ln == "panel") {
-				break;
-			}
-		}
-		return null;
-	},
-	getDlController: function(node) {
-		var dlItem = this.getDlNode(node);
-		if(!dlItem)
-			return null;
-		var window = dlItem.ownerDocument.defaultView;
-		return new window.DownloadsViewItemController(dlItem);
-	},
-	getDataItemPath: function(dataItem) {
-		var path = dataItem.file;
-		if(!path || typeof path != "string" || path.startsWith("file:/")) // Firefox 24 and older
-			path = dataItem.localFile.path;
-		return path;
-	},
+
 	stopEvent: function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 		e.stopImmediatePropagation();
 	},
-	showDownloadWindow: function(window) {
-		this.toggleDownloadPanel(window, false);
-		if(!this.dispatchAPIEvent(window, "OpenDownloadWindow")) {
-			_log("showDownloadWindow(): someone handle API event, do nothing");
-			return true;
-		}
-		// https://addons.mozilla.org/firefox/addon/downloads-window/
-		if(this.packageAvailable("downloads_window")) {
-			_log("Found Downloads Window extension, will open its window");
-			return this.openWindow(window, {
-				uri: "chrome://downloads_window/content/downloadsWindow.xul",
-				name: "downloads window",
-				features: "chrome,dialog=no,resizable,centerscreen"
-			});
-		}
-		if(
-			"PrivateBrowsingUtils" in window
-			&& window.PrivateBrowsingUtils.isWindowPrivate(window.content)
-		) {
-			_log("showDownloadWindow(): private downloads aren't supported");
-			return false;
-		}
-		// See resource://app/components/DownloadsUI.js
-		// DownloadsUI.prototype.show()
-		_log("showDownloadWindow()");
-		var toolkitUI = Components.classesByID["{7dfdf0d1-aff6-4a34-bad1-d0fe74601642}"]
-			.getService(Components.interfaces.nsIDownloadManagerUI);
-		toolkitUI.show(window/*, aDownload, aReason, aUsePrivateUI*/);
-		return true;
-	},
-	toggleDownloadPanel: function(window, show) {
-		_log("toggleDownloadPanel(" + show + ")");
-		var DownloadsPanel = window.DownloadsPanel;
-		if(show === undefined)
-			show = !DownloadsPanel.isPanelShowing;
-		else if(show == DownloadsPanel.isPanelShowing)
-			return;
-		if(show)
-			DownloadsPanel.showPanel();
-		else
-			DownloadsPanel.hidePanel();
-	},
-	openDownloadsTab: function(window) {
-		this.toggleDownloadPanel(window, false);
-		const downloadsURI = "about:downloads";
-		var gBrowser = window.gBrowser;
-		// We need to check private state for Private Tab extension
-		var pbu = "PrivateBrowsingUtils" in window && window.PrivateBrowsingUtils;
-		var isPrivate = pbu && pbu.isWindowPrivate(window.content);
-		if(!Array.some(gBrowser.visibleTabs || gBrowser.tabs, function(tab) {
-			var browser = tab.linkedBrowser;
-			if(
-				browser
-				&& browser.currentURI
-				&& browser.currentURI.spec == downloadsURI
-				&& isPrivate == (pbu && pbu.isWindowPrivate(browser.contentWindow))
-			) {
-				gBrowser.selectedTab = tab;
-				return true;
-			}
-			return false;
-		})) {
-			//gBrowser.selectedTab = gBrowser.addTab(downloadsURI);
-			// See resource://app/components/DownloadsUI.js
-			window.openUILinkIn(downloadsURI, "tab");
-		}
-	},
-	openDownloadsLibrary: function(window) {
-		this.toggleDownloadPanel(window, false);
-		// See resource://app/components/DownloadsUI.js
-		return this.openWindow(window, {
-			uri: "chrome://browser/content/places/places.xul",
-			type: "Places:Organizer",
-			features: "chrome,toolbar=yes,dialog=no,resizable",
-			args: ["Downloads"],
-			callback: function(win, alreadyOpened) {
-				if(alreadyOpened)
-					win.PlacesOrganizer.selectLeftPaneQuery("Downloads");
-			}
-		});
-	},
-	toggleDownloadsSidebar: function(window) {
-		this.toggleDownloadPanel(window, false);
-		if(!this.dispatchAPIEvent(window, "ToggleDownloadSidebar")) {
-			_log("toggleDownloadsSidebar(): someone handle API event, do nothing");
-			return true;
-		}
-		var document = window.document;
-		var sbItem = document.getElementById("menu_dmSidebar") // OmniSidebar
-			|| document.getElementById("downloads-mitem"); // All-in-One Sidebar
-		if(sbItem) {
-			// Prefer broadcaster, if available (because menuitem may be disabled)
-			// see https://github.com/Infocatcher/Download_Panel_Tweaker/issues/21
-			var observes = sbItem.getAttribute("observes");
-			sbItem = observes && document.getElementById(observes) || sbItem;
-			_log("toggleDownloadsSidebar(): found #" + sbItem.id);
-			sbItem.doCommand();
-			return;
-		}
-		var sbBrowser = document.getElementById("sidebar");
-		var wpBrowser = sbBrowser && sbBrowser.boxObject.width > 0
-			&& sbBrowser.contentDocument.getElementById("web-panels-browser");
-		if(wpBrowser && wpBrowser.currentURI.spec == "about:downloads") {
-			window.toggleSidebar();
-			return;
-		}
-		var downloadsTitle = this.getEntity(
-			["chrome://browser/locale/downloads/downloads.dtd"],
-			"downloads.title",
-			"Downloads"
-		);
-		window.openWebPanel(downloadsTitle, "about:downloads");
-	},
-
 	dispatchAPIEvent: function(window, type) {
 		var evt = window.document.createEvent("Events");
 		evt.initEvent("DownloadPanelTweaker:" + type, true, true);
@@ -896,8 +762,8 @@ var dpTweaker = {
 
 	updateDownloadsContextMenu: function(popup) {
 		_log("updateDownloadsContextMenu()");
-		var dlItem = this.getDlNode(popup.triggerNode);
-		var dlController = this.getDlController(dlItem);
+		var dlItem = this.da.getDlNode(popup.triggerNode);
+		var dlController = this.da.getDlController(dlItem);
 		Array.forEach(
 			popup.getElementsByAttribute("downloadPanelTweaker-command", "*"),
 			function(mi) {
@@ -929,130 +795,6 @@ var dpTweaker = {
 			node.removeAttribute("disabled");
 		else
 			node.setAttribute("disabled", "true");
-	},
-
-	clearDownloads: function(mi) {
-		_log("clearDownloads()");
-		if(prefs.get("clearDownloads.confirm")) {
-			var strings = {
-				"dpt.clearDownloads.confirmTitle": "Download Panel Tweaker",
-				"dpt.clearDownloads.confirmMessage": "Are you sure you want to clear ALL downloads history?",
-				"dpt.clearDownloads.dontAskAgain": "Don't ask again"
-			};
-			this.getEntities(["chrome://downloadpaneltweaker/locale/dpt.dtd"], strings);
-			var dontAsk = { value: false };
-			var ok = Services.prompt.confirmCheck(
-				mi && mi.ownerDocument.defaultView || Services.ww.activeWindow,
-				strings["dpt.clearDownloads.confirmTitle"],
-				strings["dpt.clearDownloads.confirmMessage"],
-				strings["dpt.clearDownloads.dontAskAgain"],
-				dontAsk
-			);
-			if(!ok)
-				return;
-			if(dontAsk)
-				prefs.set("clearDownloads.confirm", false);
-		}
-		try {
-			var downloads = Services.downloads;
-			downloads.canCleanUp && downloads.cleanUp();
-			downloads.canCleanUpPrivate && downloads.cleanUpPrivate();
-		}
-		catch(e) { // Firefox 26.0a1
-			_log("clearDownloads(): Services.downloads.cleanUp/cleanUpPrivate() failed:\n" + e);
-			try {
-				var global = Components.utils.import("resource://app/modules/DownloadsCommon.jsm", {});
-				if(global.DownloadsData && global.DownloadsData.removeFinished) {
-					global.DownloadsData.removeFinished();
-					_log("clearDownloads(): cleanup DownloadsData");
-				}
-				if(global.PrivateDownloadsData && global.PrivateDownloadsData.removeFinished) {
-					global.PrivateDownloadsData.removeFinished();
-					_log("clearDownloads(): cleanup PrivateDownloadsData");
-				}
-			}
-			catch(e2) {
-				Components.utils.reportError(e2);
-			}
-		}
-		Components.classes["@mozilla.org/browser/download-history;1"]
-			.getService(Components.interfaces.nsIDownloadHistory)
-			.removeAllDownloads();
-		_log("clearDownloads(): done");
-	},
-	copyReferrer: function(mi) {
-		var dlContext = mi.parentNode;
-		var dlController = this.getDlController(dlContext.triggerNode);
-		var document = mi.ownerDocument;
-		var clipHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
-			.getService(Components.interfaces.nsIClipboardHelper);
-		try {
-			var contentDoc = document.defaultView.content.document; // For Private Tab extension
-			clipHelper.copyString(dlController.dataItem.referrer, contentDoc);
-		}
-		catch(e) {
-			_log("nsIClipboardHelper.copyString(..., content.document) failed, Electrolysis?");
-			Components.utils.reportError(e);
-			clipHelper.copyString(dlController.dataItem.referrer, document);
-		}
-	},
-	removeFile: function(mi) {
-		var dlContext = mi.parentNode;
-		var dlItem = this.getDlNode(dlContext.triggerNode);
-		var dlController = this.getDlController(dlItem);
-		var dataItem = dlController.dataItem;
-		var path = this.getDataItemPath(dataItem);
-		_log("removeFile(): " + path);
-		var htmlPattern = /\.(?:[xs]?html?|xht)$/i;
-		var removeFilesDirPref = "removeFile.removeFilesDirectoryForHTML";
-		var clearHistory = prefs.get("removeFile.clearHistory");
-		try {
-			Components.utils.import("resource://gre/modules/osfile.jsm");
-			OS.File.remove(path).then(
-				function onSuccess() {
-					dlItem.removeAttribute("exists");
-					if(clearHistory)
-						this.removeFromPanel(dlController, clearHistory > 1);
-				}.bind(this),
-				Components.utils.reportError
-			);
-			if(htmlPattern.test(path) && prefs.get(removeFilesDirPref)) {
-				var filesPath = RegExp.leftContext + "_files";
-				_log("removeFile(): HTML _files directory: " + filesPath);
-				OS.File.removeDir(filesPath, { ignoreAbsent: true }).then(
-					null,
-					Components.utils.reportError
-				);
-			}
-		}
-		catch(e) { // Firefox 17
-			if((e.message || e) != "osfile.jsm cannot be used from the main thread yet")
-				Components.utils.reportError(e);
-			_log("removeFile(): will use dataItem.localFile.remove(false)");
-			var localFile = dataItem.localFile;
-			if(htmlPattern.test(localFile.leafName) && prefs.get(removeFilesDirPref)) {
-				var filesName = RegExp.leftContext + "_files";
-				var filesDir = localFile.parent.clone();
-				filesDir.append(filesName);
-				_log("removeFile(): HTML _files directory: " + filesDir.path);
-			}
-			localFile.remove(false);
-			if(clearHistory)
-				this.removeFromPanel(dlController, clearHistory > 1);
-			if(filesDir && filesDir.exists())
-				filesDir.remove(true);
-		}
-	},
-	removeFromPanel: function(dlController, clearHistory) {
-		// See chrome://browser/content/downloads/downloads.js
-		if(clearHistory) {
-			dlController.doCommand("cmd_delete");
-			_log('removeFromPanel() -> dlController.doCommand("cmd_delete")');
-		}
-		else {
-			dlController.dataItem.remove();
-			_log("removeFromPanel() -> dlController.dataItem.remove()");
-		}
 	},
 
 	setFixToolbox: function(window, enable) {
@@ -1102,50 +844,6 @@ var dpTweaker = {
 			tb.setAttribute("tabsontop", ttRoot);
 			_log("fixToolbox(): override \"tabsontop\" on #navigator-toolbox: " + tt + " => " + ttRoot);
 		}
-	},
-
-	get xcr() {
-		delete this.xcr;
-		return this.xcr = Components.classes["@mozilla.org/chrome/chrome-registry;1"]
-			.getService(Components.interfaces.nsIXULChromeRegistry);
-	},
-	packageAvailable: function(packageName) {
-		try {
-			return /^[a-z]/.test(this.xcr.getSelectedLocale(packageName));
-		}
-		catch(e) {
-		}
-		return false;
-	},
-	openWindow: function(parentWindow, options) {
-		var win = options.type && Services.wm.getMostRecentWindow(options.type)
-			|| options.name && Services.ww.getWindowByName(options.name, null)
-			|| (function() {
-				var ws = Services.wm.getEnumerator(null);
-				while(ws.hasMoreElements()) {
-					var win = ws.getNext();
-					if(win.location.href == options.uri)
-						return win;
-				}
-				return null;
-			})();
-		if(win) {
-			_log("openWindow(): already opened " + options.uri);
-			options.callback && options.callback(win, true);
-			win.focus();
-		}
-		else {
-			var openArgs = [options.uri, options.name || "", options.features || "chrome,all,dialog=0"];
-			options.args && openArgs.push.apply(openArgs, options.args);
-			win = parentWindow.openDialog.apply(parentWindow, openArgs);
-			_log("openWindow(): open " + options.uri);
-			options.callback && win.addEventListener("load", function load(e) {
-				win.removeEventListener(e.type, load, false);
-				_log("openWindow(): loaded " + options.uri);
-				options.callback(win, false);
-			}, false);
-		}
-		return win;
 	},
 
 	getEntity: function(dtds, name, dafaultVal) {

downloadsActions.js
@@ -1,0 +1,313 @@
+var downloadsActions = {
+	dpt: dpTweaker,
+
+	showDownloadWindow: function(window) {
+		this.toggleDownloadPanel(window, false);
+		if(!this.dpt.dispatchAPIEvent(window, "OpenDownloadWindow")) {
+			_log("showDownloadWindow(): someone handle API event, do nothing");
+			return true;
+		}
+		// https://addons.mozilla.org/firefox/addon/downloads-window/
+		if(this.packageAvailable("downloads_window")) {
+			_log("Found Downloads Window extension, will open its window");
+			return this.openWindow(window, {
+				uri: "chrome://downloads_window/content/downloadsWindow.xul",
+				name: "downloads window",
+				features: "chrome,dialog=no,resizable,centerscreen"
+			});
+		}
+		if(
+			"PrivateBrowsingUtils" in window
+			&& window.PrivateBrowsingUtils.isWindowPrivate(window.content)
+		) {
+			_log("showDownloadWindow(): private downloads aren't supported");
+			return false;
+		}
+		// See resource://app/components/DownloadsUI.js
+		// DownloadsUI.prototype.show()
+		_log("showDownloadWindow()");
+		var toolkitUI = Components.classesByID["{7dfdf0d1-aff6-4a34-bad1-d0fe74601642}"]
+			.getService(Components.interfaces.nsIDownloadManagerUI);
+		toolkitUI.show(window/*, aDownload, aReason, aUsePrivateUI*/);
+		return true;
+	},
+	toggleDownloadPanel: function(window, show) {
+		_log("toggleDownloadPanel(" + show + ")");
+		var DownloadsPanel = window.DownloadsPanel;
+		if(show === undefined)
+			show = !DownloadsPanel.isPanelShowing;
+		else if(show == DownloadsPanel.isPanelShowing)
+			return;
+		if(show)
+			DownloadsPanel.showPanel();
+		else
+			DownloadsPanel.hidePanel();
+	},
+	openDownloadsTab: function(window) {
+		this.toggleDownloadPanel(window, false);
+		const downloadsURI = "about:downloads";
+		var gBrowser = window.gBrowser;
+		// We need to check private state for Private Tab extension
+		var pbu = "PrivateBrowsingUtils" in window && window.PrivateBrowsingUtils;
+		var isPrivate = pbu && pbu.isWindowPrivate(window.content);
+		if(!Array.some(gBrowser.visibleTabs || gBrowser.tabs, function(tab) {
+			var browser = tab.linkedBrowser;
+			if(
+				browser
+				&& browser.currentURI
+				&& browser.currentURI.spec == downloadsURI
+				&& isPrivate == (pbu && pbu.isWindowPrivate(browser.contentWindow))
+			) {
+				gBrowser.selectedTab = tab;
+				return true;
+			}
+			return false;
+		})) {
+			//gBrowser.selectedTab = gBrowser.addTab(downloadsURI);
+			// See resource://app/components/DownloadsUI.js
+			window.openUILinkIn(downloadsURI, "tab");
+		}
+	},
+	openDownloadsLibrary: function(window) {
+		this.toggleDownloadPanel(window, false);
+		// See resource://app/components/DownloadsUI.js
+		return this.openWindow(window, {
+			uri: "chrome://browser/content/places/places.xul",
+			type: "Places:Organizer",
+			features: "chrome,toolbar=yes,dialog=no,resizable",
+			args: ["Downloads"],
+			callback: function(win, alreadyOpened) {
+				if(alreadyOpened)
+					win.PlacesOrganizer.selectLeftPaneQuery("Downloads");
+			}
+		});
+	},
+	toggleDownloadsSidebar: function(window) {
+		this.toggleDownloadPanel(window, false);
+		if(!this.dpt.dispatchAPIEvent(window, "ToggleDownloadSidebar")) {
+			_log("toggleDownloadsSidebar(): someone handle API event, do nothing");
+			return true;
+		}
+		var document = window.document;
+		var sbItem = document.getElementById("menu_dmSidebar") // OmniSidebar
+			|| document.getElementById("downloads-mitem"); // All-in-One Sidebar
+		if(sbItem) {
+			// Prefer broadcaster, if available (because menuitem may be disabled)
+			// see https://github.com/Infocatcher/Download_Panel_Tweaker/issues/21
+			var observes = sbItem.getAttribute("observes");
+			sbItem = observes && document.getElementById(observes) || sbItem;
+			_log("toggleDownloadsSidebar(): found #" + sbItem.id);
+			sbItem.doCommand();
+			return;
+		}
+		var sbBrowser = document.getElementById("sidebar");
+		var wpBrowser = sbBrowser && sbBrowser.boxObject.width > 0
+			&& sbBrowser.contentDocument.getElementById("web-panels-browser");
+		if(wpBrowser && wpBrowser.currentURI.spec == "about:downloads") {
+			window.toggleSidebar();
+			return;
+		}
+		var downloadsTitle = this.dpt.getEntity(
+			["chrome://browser/locale/downloads/downloads.dtd"],
+			"downloads.title",
+			"Downloads"
+		);
+		window.openWebPanel(downloadsTitle, "about:downloads");
+	},
+
+	clearDownloads: function(mi) {
+		_log("clearDownloads()");
+		if(prefs.get("clearDownloads.confirm")) {
+			var strings = {
+				"dpt.clearDownloads.confirmTitle": "Download Panel Tweaker",
+				"dpt.clearDownloads.confirmMessage": "Are you sure you want to clear ALL downloads history?",
+				"dpt.clearDownloads.dontAskAgain": "Don't ask again"
+			};
+			this.dpt.getEntities(["chrome://downloadpaneltweaker/locale/dpt.dtd"], strings);
+			var dontAsk = { value: false };
+			var ok = Services.prompt.confirmCheck(
+				mi && mi.ownerDocument.defaultView || Services.ww.activeWindow,
+				strings["dpt.clearDownloads.confirmTitle"],
+				strings["dpt.clearDownloads.confirmMessage"],
+				strings["dpt.clearDownloads.dontAskAgain"],
+				dontAsk
+			);
+			if(!ok)
+				return;
+			if(dontAsk)
+				prefs.set("clearDownloads.confirm", false);
+		}
+		try {
+			var downloads = Services.downloads;
+			downloads.canCleanUp && downloads.cleanUp();
+			downloads.canCleanUpPrivate && downloads.cleanUpPrivate();
+		}
+		catch(e) { // Firefox 26.0a1
+			_log("clearDownloads(): Services.downloads.cleanUp/cleanUpPrivate() failed:\n" + e);
+			try {
+				var global = Components.utils.import("resource://app/modules/DownloadsCommon.jsm", {});
+				if(global.DownloadsData && global.DownloadsData.removeFinished) {
+					global.DownloadsData.removeFinished();
+					_log("clearDownloads(): cleanup DownloadsData");
+				}
+				if(global.PrivateDownloadsData && global.PrivateDownloadsData.removeFinished) {
+					global.PrivateDownloadsData.removeFinished();
+					_log("clearDownloads(): cleanup PrivateDownloadsData");
+				}
+			}
+			catch(e2) {
+				Components.utils.reportError(e2);
+			}
+		}
+		Components.classes["@mozilla.org/browser/download-history;1"]
+			.getService(Components.interfaces.nsIDownloadHistory)
+			.removeAllDownloads();
+		_log("clearDownloads(): done");
+	},
+	copyReferrer: function(mi) {
+		var dlContext = mi.parentNode;
+		var dlController = this.getDlController(dlContext.triggerNode);
+		var document = mi.ownerDocument;
+		var clipHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+			.getService(Components.interfaces.nsIClipboardHelper);
+		try {
+			var contentDoc = document.defaultView.content.document; // For Private Tab extension
+			clipHelper.copyString(dlController.dataItem.referrer, contentDoc);
+		}
+		catch(e) {
+			_log("nsIClipboardHelper.copyString(..., content.document) failed, Electrolysis?");
+			Components.utils.reportError(e);
+			clipHelper.copyString(dlController.dataItem.referrer, document);
+		}
+	},
+	removeFile: function(mi) {
+		var dlContext = mi.parentNode;
+		var dlItem = this.getDlNode(dlContext.triggerNode);
+		var dlController = this.getDlController(dlItem);
+		var dataItem = dlController.dataItem;
+		var path = this.getDataItemPath(dataItem);
+		_log("removeFile(): " + path);
+		var htmlPattern = /\.(?:[xs]?html?|xht)$/i;
+		var removeFilesDirPref = "removeFile.removeFilesDirectoryForHTML";
+		var clearHistory = prefs.get("removeFile.clearHistory");
+		try {
+			Components.utils.import("resource://gre/modules/osfile.jsm");
+			OS.File.remove(path).then(
+				function onSuccess() {
+					dlItem.removeAttribute("exists");
+					if(clearHistory)
+						this.removeFromPanel(dlController, clearHistory > 1);
+				}.bind(this),
+				Components.utils.reportError
+			);
+			if(htmlPattern.test(path) && prefs.get(removeFilesDirPref)) {
+				var filesPath = RegExp.leftContext + "_files";
+				_log("removeFile(): HTML _files directory: " + filesPath);
+				OS.File.removeDir(filesPath, { ignoreAbsent: true }).then(
+					null,
+					Components.utils.reportError
+				);
+			}
+		}
+		catch(e) { // Firefox 17
+			if((e.message || e) != "osfile.jsm cannot be used from the main thread yet")
+				Components.utils.reportError(e);
+			_log("removeFile(): will use dataItem.localFile.remove(false)");
+			var localFile = dataItem.localFile;
+			if(htmlPattern.test(localFile.leafName) && prefs.get(removeFilesDirPref)) {
+				var filesName = RegExp.leftContext + "_files";
+				var filesDir = localFile.parent.clone();
+				filesDir.append(filesName);
+				_log("removeFile(): HTML _files directory: " + filesDir.path);
+			}
+			localFile.remove(false);
+			if(clearHistory)
+				this.removeFromPanel(dlController, clearHistory > 1);
+			if(filesDir && filesDir.exists())
+				filesDir.remove(true);
+		}
+	},
+	removeFromPanel: function(dlController, clearHistory) {
+		// See chrome://browser/content/downloads/downloads.js
+		if(clearHistory) {
+			dlController.doCommand("cmd_delete");
+			_log('removeFromPanel() -> dlController.doCommand("cmd_delete")');
+		}
+		else {
+			dlController.dataItem.remove();
+			_log("removeFromPanel() -> dlController.dataItem.remove()");
+		}
+	},
+
+	getDlNode: function(node) {
+		for(; node; node = node.parentNode) {
+			var ln = node.localName;
+			if(ln == "richlistitem") {
+				if(node.getAttribute("type") == "download")
+					return node;
+				break;
+			}
+			else if(ln == "panel") {
+				break;
+			}
+		}
+		return null;
+	},
+	getDlController: function(node) {
+		var dlItem = this.getDlNode(node);
+		if(!dlItem)
+			return null;
+		var window = dlItem.ownerDocument.defaultView;
+		return new window.DownloadsViewItemController(dlItem);
+	},
+	getDataItemPath: function(dataItem) {
+		var path = dataItem.file;
+		if(!path || typeof path != "string" || path.startsWith("file:/")) // Firefox 24 and older
+			path = dataItem.localFile.path;
+		return path;
+	},
+
+	get xcr() {
+		delete this.xcr;
+		return this.xcr = Components.classes["@mozilla.org/chrome/chrome-registry;1"]
+			.getService(Components.interfaces.nsIXULChromeRegistry);
+	},
+	packageAvailable: function(packageName) {
+		try {
+			return /^[a-z]/.test(this.xcr.getSelectedLocale(packageName));
+		}
+		catch(e) {
+		}
+		return false;
+	},
+	openWindow: function(parentWindow, options) {
+		var win = options.type && Services.wm.getMostRecentWindow(options.type)
+			|| options.name && Services.ww.getWindowByName(options.name, null)
+			|| (function() {
+				var ws = Services.wm.getEnumerator(null);
+				while(ws.hasMoreElements()) {
+					var win = ws.getNext();
+					if(win.location.href == options.uri)
+						return win;
+				}
+				return null;
+			})();
+		if(win) {
+			_log("openWindow(): already opened " + options.uri);
+			options.callback && options.callback(win, true);
+			win.focus();
+		}
+		else {
+			var openArgs = [options.uri, options.name || "", options.features || "chrome,all,dialog=0"];
+			options.args && openArgs.push.apply(openArgs, options.args);
+			win = parentWindow.openDialog.apply(parentWindow, openArgs);
+			_log("openWindow(): open " + options.uri);
+			options.callback && win.addEventListener("load", function load(e) {
+				win.removeEventListener(e.type, load, false);
+				_log("openWindow(): loaded " + options.uri);
+				options.callback(win, false);
+			}, false);
+		}
+		return win;
+	}
+};

commit 369d44da030c6b5e3e48b9e10201a7e4aab88f9f

Correctly handle middle-click in panel (regression in bf01d217c5d23046b9eea6fd90b857bf023f7071 commit) (#24)

bootstrap.js
@@ -618,7 +618,7 @@ var dpTweaker = {
 		var dlController = this.da.getDlController(e.target);
 		if(!dlController)
 			return;
-		this.removeFromPanel(dlController, prefs.get("middleClickToRemoveFromPanel.clearHistory"));
+		this.da.removeFromPanel(dlController, prefs.get("middleClickToRemoveFromPanel.clearHistory"));
 		this.stopEvent(e);
 	},
 