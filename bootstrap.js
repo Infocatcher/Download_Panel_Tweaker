f92b8ee6b6c8ad485759669dfd9594408be010a0
Infocatcher <infocatcher.dev.null@gmail.com>
2014-06-05 10:16:45 +0400

    Remove [attention="true"] attribute from #downloads-indicator (Firefox 26 and older) (#23)

--- bootstrap.js
+++ bootstrap.js
@@ -858,20 +858,54 @@ var dpTweaker = {
 		return window.document.getElementById(id)
 			|| window.gNavToolbox.palette.getElementsByAttribute("id", id)[0];
 	},
-	tweakDlButton: function(window, tweak, forceDestroy) {
-		var dlBtn = this.getButton(window, "downloads-button");
+	tweakDlButton: function(window, tweak, forceDestroy, dlInd) {
+		var dlBtn = dlInd || this.getButton(window, "downloads-button");
 		if(!dlBtn) {
 			_log("tweakDlButton(): button not found!");
 			return;
 		}
-		_log("tweakDlButton(" + tweak + ")");
+		_log("tweakDlButton(" + tweak + "): #" + dlBtn.id);
+		if(this.fxVersion < 27 && !dlInd) {
+			dlInd = this.getButton(window, "downloads-indicator");
+			if(dlInd)
+				this.tweakDlButton(window, tweak, forceDestroy, dlInd);
+			if(!dlInd || !tweak && forceDestroy)
+				this.waitForDlIndicator(window, dlBtn, tweak);
+		}
 		this.dontHighlightButton(window, dlBtn, tweak && prefs.get("dontHighlightButton"), forceDestroy);
+	},
+	waitForDlIndicator: function(window, dlBtn, wait) {
+		// Wait for #downloads-indicator (Firefox 26 and older)
+		var key = "_downloadPanelTweaker_mutationObserverWaitDlIndicator";
+		if(wait == key in dlBtn)
+			return;
+		_log("waitForDlIndicator(" + wait + ")");
+		if(wait) {
+			var mo = dlBtn[key] = new window.MutationObserver(function(mutations) {
+				var dlInd = this.getButton(window, "downloads-indicator");
+				if(dlInd) {
+					_log("waitForDlIndicator(): appears #downloads-indicator");
+					delete dlBtn[key];
+					mo.disconnect();
+					this.tweakDlButton(window, true, false, dlInd);
+				}
+			}.bind(this));
+			mo.observe(dlBtn, {
+				attributes: true,
+				attributeFilter: ["collapsed"]
+			});
+		}
+		else {
+			var mo = dlBtn[key];
+			delete dlBtn[key];
+			mo.disconnect();
+		}
 	},
 	dontHighlightButton: function(window, dlBtn, dontHL, forceDestroy) {
 		var key = "_downloadPanelTweaker_mutationObserverDontHL";
 		if(dontHL == key in dlBtn)
 			return;
-		_log("dontHighlightButton(" + dontHL + ")");
+		_log("dontHighlightButton(" + dontHL + ") #" + dlBtn.id);
 		if(dontHL) {
 			this.removeDlAttention(dlBtn);
 			var mo = dlBtn[key] = new window.MutationObserver(function(mutations) {
