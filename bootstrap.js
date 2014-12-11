Infocatcher/Download_Panel_Tweaker

commit 185606e23140fc69f7062658034996055e171232

Move code around styles into separate file (#31)

bootstrap.js
@@ -39,7 +39,7 @@ var dpTweaker = {
 		for(var window in this.windows)
 			this.initWindow(window, reason);
 		if(!this._tweakStyleLoaded && Services.wm.getMostRecentWindow("Places:Organizer"))
-			this.loadStyles(true);
+			this.stl.loadStyles(true);
 		Services.ww.registerNotification(this);
 
 		delay(function() {
@@ -56,7 +56,7 @@ var dpTweaker = {
 		this.initialized = false;
 
 		if(reason != APP_SHUTDOWN) {
-			this.loadStyles(false);
+			this.stl.loadStyles(false);
 			if(prefs.get("showDownloadRate"))
 				this.showDownloadRate(false);
 			if(prefs.get("decolorizePausedProgress"))
@@ -90,6 +90,8 @@ var dpTweaker = {
 			patcher.destroy();
 			Components.utils.unload("chrome://downloadpaneltweaker/content/patcher.jsm");
 		}
+		if("downloadsStyles" in global)
+			this.stl.dpt = null;
 		if("downloadsActions" in global)
 			this.da.dpt = null;
 		if("downloadsPanel" in global)
@@ -120,7 +122,7 @@ var dpTweaker = {
 
 	initWindow: function(window, reason) {
 		if(reason == WINDOW_LOADED && this.isLibraryWindow(window)) {
-			this.loadStyles(true);
+			this.stl.loadStyles(true);
 			return;
 		}
 		if(reason == WINDOW_LOADED && !this.isTargetWindow(window))
@@ -153,7 +155,7 @@ var dpTweaker = {
 				this.setFixToolbox(window, true);
 		}.bind(this), 10);
 		window.setTimeout(function() {
-			this.loadStyles(true);
+			this.stl.loadStyles(true);
 		}.bind(this), 50);
 	},
 	destroyWindow: function(window, reason) {
@@ -193,6 +195,7 @@ var dpTweaker = {
 			yield ws.getNext();
 	},
 
+	get stl() { return this.lazy("stl", "downloadsStyles");       },
 	get de()  { return this.lazy("de",  "downloadsEnhancements"); },
 	get da()  { return this.lazy("da",  "downloadsActions");      },
 	get dp()  { return this.lazy("dp",  "downloadsPanel");        },
@@ -203,176 +206,9 @@ var dpTweaker = {
 		delete this[prop];
 		return this[prop] = global[name];
 	},
-	get sss() {
-		delete this.sss;
-		return this.sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
-			.getService(Components.interfaces.nsIStyleSheetService);
-	},
 	get fxVersion() {
 		delete this.fxVersion;
 		return this.fxVersion = parseFloat(Services.appinfo.version);
-	},
-	newCssURI: function(cssStr) {
-		cssStr = this.trimCSSString(cssStr);
-		return Services.io.newURI("data:text/css," + encodeURIComponent(cssStr), null, null);
-	},
-	trimCSSString: function(s) {
-		var spaces = s.match(/^[ \t]*/)[0];
-		return s.replace(new RegExp("^" + spaces, "mg"), "");
-	},
-	loadStyles: function(add) {
-		this.loadCompactStyle(add ? prefs.get("compactDownloads") : 0);
-		this.loadTweakStyle(add);
-	},
-	_compactStyleLoaded: false,
-	_forceCompactStyleLoaded: false,
-	loadCompactStyle: function(compact) {
-		var loadCompact = compact == 1;
-		if(loadCompact != this._compactStyleLoaded) {
-			this._compactStyleLoaded = loadCompact;
-			this.loadSheet(
-				Services.io.newURI("chrome://downloadpaneltweaker/content/compactDownloads.css", null, null),
-				loadCompact
-			);
-			_log((loadCompact ? "Load" : "Unload") + " compactDownloads.css");
-		}
-		var loadForceCompact = compact == 2;
-		if(loadForceCompact != this._forceCompactStyleLoaded) {
-			this._forceCompactStyleLoaded = loadForceCompact;
-			this.loadSheet(
-				Services.io.newURI("chrome://downloadpaneltweaker/content/compactDownloadsForce.css", null, null),
-				loadForceCompact
-			);
-			_log((loadForceCompact ? "Load" : "Unload") + " compactDownloadsForce.css");
-		}
-	},
-	_tweakStyleLoaded: false,
-	tweakCssURI: null,
-	minPanelWidth: 5,
-	minPanelHeight: 30,
-	minProgressBarHeight: 6,
-	maxProgressBarHeight: 50,
-	pausedAttr: "downloadPanelTweaker_paused",
-	loadTweakStyle: function(add) {
-		if(add == this._tweakStyleLoaded)
-			return;
-		this._tweakStyleLoaded = add;
-		var cssURI;
-		if(add) {
-			var panelWidth = Math.max(this.minPanelWidth, prefs.get("panelWidth", 60));
-			var panelMaxHeight = prefs.get("panelMaxHeight", 0);
-			var panelMaxHeightVal = panelMaxHeight <= 0
-				? "none"
-				: Math.max(this.minPanelHeight, panelMaxHeight) + "px";
-			var pbHeight = Math.max(this.minProgressBarHeight, Math.min(this.maxProgressBarHeight,
-				prefs.get("progressBarHeight", 10)
-			));
-			var containerSelector = this.fxVersion >= 20
-				? ".downloadContainer"
-				: ".download-state > vbox";
-			var tdsPrefix = "-moz-";
-			var window = Services.wm.getMostRecentWindow(null);
-			if(window && "textDecorationStyle" in window.document.documentElement.style)
-				tdsPrefix = "";
-			var grayscaleFilter = this.fxVersion >= 36
-				? "grayscale(1)"
-				: 'url("chrome://mozapps/skin/extensions/extensions.svg#greyscale")';
-			var cssStr = '\
-				/* Download Panel Tweaker */\n\
-				@namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul");\n\
-				@-moz-document url("chrome://browser/content/browser.xul"),\n\
-					url("chrome://browser/content/places/places.xul"),\n\
-					url("about:downloads"),\n\
-					url("chrome://browser/content/downloads/contentAreaDownloadsView.xul") {\n\
-					#downloadsListBox {\n\
-						/* Set width for Firefox < 20 and NASA Night Launch theme */\n\
-						width: auto !important;\n\
-						min-width: 0 !important;\n\
-						max-width: none !important;\n\
-						max-height: ' + panelMaxHeightVal + ' !important;\n\
-					}\n\
-					' + containerSelector + ' {\n\
-						width: ' + panelWidth + 'ch !important;\n\
-						min-width: 0 !important;\n\
-					}\n\
-					#downloadsSummaryDescription,\n\
-					#downloadsSummaryDetails {\n\
-						width: ' + panelWidth + 'ch !important;\n\
-						min-width: 0 !important;\n\
-					}\n\
-					.downloadTarget {\n\
-						min-width: 5ch !important;\n\
-					}\n\
-					.downloadProgress {\n\
-						min-width: 20px !important;\n\
-						min-height: ' + pbHeight + 'px !important;\n\
-						height: ' + pbHeight + 'px !important;\n\
-					}\n\
-					.downloadProgress > .progress-bar {\n\
-						height: auto !important;\n\
-						min-height: 2px !important;\n\
-						max-height: ' + pbHeight + 'px !important;\n\
-					}' + (
-						prefs.get("decolorizePausedProgress")
-						? '\n\
-					/* Paused downloads */\n\
-					.download-state[state="4"] .downloadProgress,\n\
-					#downloadsSummary[' + this.pausedAttr + '] .downloadProgress {\n\
-						filter: ' + grayscaleFilter + ';\n\
-					}\n\
-					.download-state[state="4"] .downloadProgress > .progress-bar,\n\
-					.download-state[state="4"] .downloadProgress > .progress-remainder,\n\
-					#downloadsSummary[' + this.pausedAttr + '] .downloadProgress > .progress-bar,\n\
-					#downloadsSummary[' + this.pausedAttr + '] .downloadProgress > .progress-remainder {\n\
-						opacity: 0.85;\n\
-					}'
-						: ""
-					) + '\n\
-				}\n\
-				@-moz-document url("about:addons"),\n\
-					url("chrome://mozapps/content/extensions/extensions.xul") {\n\
-					.downloadPanelTweaker-item[tooltiptext] {\n\
-						text-decoration: underline;\n\
-						' + tdsPrefix + 'text-decoration-style: dotted;\n\
-					}\n\
-					.downloadPanelTweaker-indent .preferences-title {\n\
-						-moz-margin-start: 4ch !important;\n\
-					}\n\
-				}';
-			cssURI = this.tweakCssURI = this.newCssURI(cssStr);
-		}
-		else {
-			cssURI = this.tweakCssURI;
-		}
-		this.loadSheet(cssURI, add);
-		_log("loadTweakStyle(" + add + ")");
-	},
-	reloadTweakStyle: function() {
-		this.loadTweakStyle(false);
-		this.loadTweakStyle(true);
-	},
-	_reloadTweakStyleTimer: null,
-	reloadTweakStyleProxy: function() {
-		var timer = this._reloadTweakStyleTimer;
-		if(timer)
-			timer.cancel();
-		else {
-			timer = this._reloadTweakStyleTimer = Components.classes["@mozilla.org/timer;1"]
-				.createInstance(Components.interfaces.nsITimer);
-		}
-		timer.init(function() {
-			this._reloadTweakStyleTimer = null;
-			this.reloadTweakStyle();
-		}.bind(this), 300, timer.TYPE_ONE_SHOT);
-	},
-	loadSheet: function(cssURI, load) {
-		var sss = this.sss;
-		if(load == sss.sheetRegistered(cssURI, sss.USER_SHEET))
-			return;
-		if(load)
-			sss.loadAndRegisterSheet(cssURI, sss.USER_SHEET);
-		else
-			sss.unregisterSheet(cssURI, sss.USER_SHEET);
 	},
 
 	minItemCountLimit: 1,
@@ -454,6 +290,7 @@ var dpTweaker = {
 		}
 	},
 
+	pausedAttr: "downloadPanelTweaker_paused",
 	_downloadsSummaryPatched: false,
 	showPausedDownloadsSummary: function(patch) {
 		if(patch == this._downloadsSummaryPatched)
@@ -735,11 +572,11 @@ var dpTweaker = {
 		) {
 			if(
 				pName == "panelWidth"
-					&& this.wrongPref(pName, pVal, this.minPanelWidth, 10e3)
+					&& this.wrongPref(pName, pVal, this.stl.minPanelWidth, 10e3)
 				|| pName == "progressBarHeight"
-					&& this.wrongPref(pName, pVal, this.minProgressBarHeight, this.maxProgressBarHeight)
+					&& this.wrongPref(pName, pVal, this.stl.minProgressBarHeight, this.stl.maxProgressBarHeight)
 				|| pName == "panelMaxHeight"
-					&& this.wrongPref(pName, pVal, this.minPanelHeight, 100e3, true)
+					&& this.wrongPref(pName, pVal, this.stl.minPanelHeight, 100e3, true)
 			)
 				return;
 			if(pName == "decolorizePausedProgress") {
@@ -747,7 +584,7 @@ var dpTweaker = {
 				for(var window in this.windows)
 					this.updateDownloadsSummary(window.document, pVal);
 			}
-			this.reloadTweakStyleProxy();
+			this.stl.reloadTweakStyleProxy();
 		}
 		else if(pName == "showFullPathInTooltip")
 			!pVal && this.dp.restoreAllDlItemsTooltips();

downloadsStyles.js
@@ -1,0 +1,170 @@
+var downloadsStyles = {
+	dpt: dpTweaker,
+
+	get sss() {
+		delete this.sss;
+		return this.sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
+			.getService(Components.interfaces.nsIStyleSheetService);
+	},
+	newCssURI: function(cssStr) {
+		cssStr = this.trimCSSString(cssStr);
+		return Services.io.newURI("data:text/css," + encodeURIComponent(cssStr), null, null);
+	},
+	trimCSSString: function(s) {
+		var spaces = s.match(/^[ \t]*/)[0];
+		return s.replace(new RegExp("^" + spaces, "mg"), "");
+	},
+	loadStyles: function(add) {
+		this.loadCompactStyle(add ? prefs.get("compactDownloads") : 0);
+		this.loadTweakStyle(add);
+	},
+	_compactStyleLoaded: false,
+	_forceCompactStyleLoaded: false,
+	loadCompactStyle: function(compact) {
+		var loadCompact = compact == 1;
+		if(loadCompact != this._compactStyleLoaded) {
+			this._compactStyleLoaded = loadCompact;
+			this.loadSheet(
+				Services.io.newURI("chrome://downloadpaneltweaker/content/compactDownloads.css", null, null),
+				loadCompact
+			);
+			_log((loadCompact ? "Load" : "Unload") + " compactDownloads.css");
+		}
+		var loadForceCompact = compact == 2;
+		if(loadForceCompact != this._forceCompactStyleLoaded) {
+			this._forceCompactStyleLoaded = loadForceCompact;
+			this.loadSheet(
+				Services.io.newURI("chrome://downloadpaneltweaker/content/compactDownloadsForce.css", null, null),
+				loadForceCompact
+			);
+			_log((loadForceCompact ? "Load" : "Unload") + " compactDownloadsForce.css");
+		}
+	},
+	_tweakStyleLoaded: false,
+	tweakCssURI: null,
+	minPanelWidth: 5,
+	minPanelHeight: 30,
+	minProgressBarHeight: 6,
+	maxProgressBarHeight: 50,
+	loadTweakStyle: function(add) {
+		if(add == this._tweakStyleLoaded)
+			return;
+		this._tweakStyleLoaded = add;
+		var cssURI;
+		if(add) {
+			var panelWidth = Math.max(this.minPanelWidth, prefs.get("panelWidth", 60));
+			var panelMaxHeight = prefs.get("panelMaxHeight", 0);
+			var panelMaxHeightVal = panelMaxHeight <= 0
+				? "none"
+				: Math.max(this.minPanelHeight, panelMaxHeight) + "px";
+			var pbHeight = Math.max(this.minProgressBarHeight, Math.min(this.maxProgressBarHeight,
+				prefs.get("progressBarHeight", 10)
+			));
+			var containerSelector = this.dpt.fxVersion >= 20
+				? ".downloadContainer"
+				: ".download-state > vbox";
+			var tdsPrefix = "-moz-";
+			var window = Services.wm.getMostRecentWindow(null);
+			if(window && "textDecorationStyle" in window.document.documentElement.style)
+				tdsPrefix = "";
+			var grayscaleFilter = this.dpt.fxVersion >= 36
+				? "grayscale(1)"
+				: 'url("chrome://mozapps/skin/extensions/extensions.svg#greyscale")';
+			var cssStr = '\
+				/* Download Panel Tweaker */\n\
+				@namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul");\n\
+				@-moz-document url("chrome://browser/content/browser.xul"),\n\
+					url("chrome://browser/content/places/places.xul"),\n\
+					url("about:downloads"),\n\
+					url("chrome://browser/content/downloads/contentAreaDownloadsView.xul") {\n\
+					#downloadsListBox {\n\
+						/* Set width for Firefox < 20 and NASA Night Launch theme */\n\
+						width: auto !important;\n\
+						min-width: 0 !important;\n\
+						max-width: none !important;\n\
+						max-height: ' + panelMaxHeightVal + ' !important;\n\
+					}\n\
+					' + containerSelector + ' {\n\
+						width: ' + panelWidth + 'ch !important;\n\
+						min-width: 0 !important;\n\
+					}\n\
+					#downloadsSummaryDescription,\n\
+					#downloadsSummaryDetails {\n\
+						width: ' + panelWidth + 'ch !important;\n\
+						min-width: 0 !important;\n\
+					}\n\
+					.downloadTarget {\n\
+						min-width: 5ch !important;\n\
+					}\n\
+					.downloadProgress {\n\
+						min-width: 20px !important;\n\
+						min-height: ' + pbHeight + 'px !important;\n\
+						height: ' + pbHeight + 'px !important;\n\
+					}\n\
+					.downloadProgress > .progress-bar {\n\
+						height: auto !important;\n\
+						min-height: 2px !important;\n\
+						max-height: ' + pbHeight + 'px !important;\n\
+					}' + (
+						prefs.get("decolorizePausedProgress")
+						? '\n\
+					/* Paused downloads */\n\
+					.download-state[state="4"] .downloadProgress,\n\
+					#downloadsSummary[' + this.dpt.pausedAttr + '] .downloadProgress {\n\
+						filter: ' + grayscaleFilter + ';\n\
+					}\n\
+					.download-state[state="4"] .downloadProgress > .progress-bar,\n\
+					.download-state[state="4"] .downloadProgress > .progress-remainder,\n\
+					#downloadsSummary[' + this.dpt.pausedAttr + '] .downloadProgress > .progress-bar,\n\
+					#downloadsSummary[' + this.dpt.pausedAttr + '] .downloadProgress > .progress-remainder {\n\
+						opacity: 0.85;\n\
+					}'
+						: ""
+					) + '\n\
+				}\n\
+				@-moz-document url("about:addons"),\n\
+					url("chrome://mozapps/content/extensions/extensions.xul") {\n\
+					.downloadPanelTweaker-item[tooltiptext] {\n\
+						text-decoration: underline;\n\
+						' + tdsPrefix + 'text-decoration-style: dotted;\n\
+					}\n\
+					.downloadPanelTweaker-indent .preferences-title {\n\
+						-moz-margin-start: 4ch !important;\n\
+					}\n\
+				}';
+			cssURI = this.tweakCssURI = this.newCssURI(cssStr);
+		}
+		else {
+			cssURI = this.tweakCssURI;
+		}
+		this.loadSheet(cssURI, add);
+		_log("loadTweakStyle(" + add + ")");
+	},
+	reloadTweakStyle: function() {
+		this.loadTweakStyle(false);
+		this.loadTweakStyle(true);
+	},
+	_reloadTweakStyleTimer: null,
+	reloadTweakStyleProxy: function() {
+		var timer = this._reloadTweakStyleTimer;
+		if(timer)
+			timer.cancel();
+		else {
+			timer = this._reloadTweakStyleTimer = Components.classes["@mozilla.org/timer;1"]
+				.createInstance(Components.interfaces.nsITimer);
+		}
+		timer.init(function() {
+			this._reloadTweakStyleTimer = null;
+			this.reloadTweakStyle();
+		}.bind(this), 300, timer.TYPE_ONE_SHOT);
+	},
+	loadSheet: function(cssURI, load) {
+		var sss = this.sss;
+		if(load == sss.sheetRegistered(cssURI, sss.USER_SHEET))
+			return;
+		if(load)
+			sss.loadAndRegisterSheet(cssURI, sss.USER_SHEET);
+		else
+			sss.unregisterSheet(cssURI, sss.USER_SHEET);
+	}
+};

commit cdc19d56ceae96e6d097e2fa5e5179a362dfec7f

Correctly change styles around "compactDownloads" preference (broken in 185606e23140fc69f7062658034996055e171232 commit) (#31)

bootstrap.js
@@ -552,7 +552,7 @@ var dpTweaker = {
 
 	prefChanged: function(pName, pVal) {
 		if(pName == "compactDownloads")
-			this.loadCompactStyle(pVal);
+			this.stl.loadCompactStyle(pVal);
 		else if(pName == "showDownloadRate") {
 			this.showDownloadRate(pVal);
 			for(var window in this.windows)